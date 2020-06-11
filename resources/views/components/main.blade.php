28b332eeed0e5469324bc23902b84b70b2b23e23
evgeniyPP <aysanru@gmail.com>
2020-06-11 22:49:54 +0300

    Main: Download links added

--- resources/views/components/main.blade.php
+++ resources/views/components/main.blade.php
@@ -1,12 +1,18 @@
 <div>
-    <x-navbar />
-    <section class="text-gray-700 body-font mt-16">
-        <div class="container mx-auto flex px-5 py-8 items-center justify-center flex-col">
-          <img class="md:w-1/6 sm:w-2/6 w-3/6 mb-4 object-cover object-center rounded-full" alt="avatar" src="storage/avatar.png">
-          <div class="text-center lg:w-2/3 w-full">
-            <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Евгений Петрянкин</h1>
-            <h4 class="mb-8 text-xl font-medium sm:text-2xl">Web-разработчик</h4>
-          </div>
-        </div>
-      </section>
+  <x-navbar />
+  <section class="text-gray-700 body-font mt-16">
+    <div class="hidden sm:block sm:flex sm:w-full sm:justify-end sm:px-6">
+        <a href="#" class="mt-1 px-3 py-2 rounded-md text-sm font-medium text-gray-500 hover:text-gray-700 focus:outline-none focus:text-gray-700 transition duration-150 ease-in-out">Скачать резюме (.docx)
+        </a>
+        <a href="#" class="mt-1 px-3 py-2 rounded-md text-sm font-medium text-gray-500 hover:text-gray-700  focus:outline-none focus:text-gray-700 transition duration-150 ease-in-out">Скачать эту страницу (.pdf)
+        </a>
+      </div>
+    <div class="container mx-auto flex px-5 py-8 items-center justify-center flex-col">
+      <img class="md:w-1/6 sm:w-2/6 w-3/6 mb-4 object-cover object-center rounded-full" alt="avatar" src="storage/avatar.png">
+      <div class="text-center lg:w-2/3 w-full">
+        <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Евгений Петрянкин</h1>
+        <h4 class="mb-8 text-xl font-medium sm:text-2xl">Web-разработчик</h4>
+      </div>
+    </div>
+  </section>
 </div>
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -6,17 +6,17 @@
           {{-- Mobile menu button --}}
           <button x-on:click="isOpen = !isOpen" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white transition duration-150 ease-in-out" aria-label="Main menu" aria-expanded="false">
             {{-- Icon when menu is closed. --}}
-            <svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <svg x-bind:class="{ 'hidden': isOpen }" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
             </svg>
             {{-- Icon when menu is open. --}}
-            <svg class="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <svg x-bind:class="{ 'hidden': !isOpen }" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
             </svg>
           </button>
         </div>
         <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-end">
-          <div class="hidden sm:block sm:ml-6">
+          <div class="hidden sm:block">
             <div class="flex">
               <a href="#" class="px-3 py-2 rounded-md text-sm font-medium leading-5 text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Главная
               </a>
@@ -32,12 +32,8 @@
       </div>
     </div>
 
-    {{--
-            Mobile menu, toggle classes based on menu state.
-
-            Menu open: "block", Menu closed: "hidden"
-        --}}
     <div x-bind:class="{ 'hidden': !isOpen }" class="sm:hidden">
+      <div class="border-t border-gray-700"></div>
       <div class="px-2 pt-2 pb-3">
         <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Главная
         </a>
@@ -46,6 +42,11 @@
         <a href="#" class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Проекты
         </a>
         <a href="#" class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Контакты
+        </a>
+        <div class="border-t border-gray-700 my-2"></div>
+        <a href="#" class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Скачать резюме (.docx)
+        </a>
+        <a href="#" class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Скачать эту страницу (.pdf)
         </a>
       </div>
     </div>
