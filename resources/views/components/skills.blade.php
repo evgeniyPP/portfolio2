41722d77bb154efbef03d2260b0925951f1a0362
evgeniyPP <aysanru@gmail.com>
2020-06-12 21:20:13 +0300

    Fix: Skills subheader size changed

--- resources/views/components/skills.blade.php
+++ resources/views/components/skills.blade.php
@@ -1,14 +1,14 @@
 <section class="text-gray-700 body-font">
   <div class="container px-5 py-24 mx-auto">
     <h1 class="mb-8 text-2xl font-medium text-center text-gray-900 sm:text-3xl title-font">Мои навыки</h1>
-    <h4 class="mb-4 text-xl font-medium text-center sm:text-2xl">Основные</h4>
+    <h4 class="mb-4 text-lg font-medium text-center sm:text-xl">Основные</h4>
     <div class="flex flex-wrap mb-6 -mx-2 lg:w-4/5 sm:mx-auto">
       @foreach($mainSkills as $skill)
         <x-one-skill :skill="$skill" />
       @endforeach
     </div>
 
-    <h4 class="mb-4 text-xl font-medium text-center sm:text-2xl">Дополнительные</h4>
+    <h4 class="mb-4 text-lg font-medium text-center sm:text-xl">Дополнительные</h4>
     <div x-data="{ showAll: false }" class="flex flex-wrap -mx-2 lg:w-4/5 sm:mx-auto sm:mb-2">
       @foreach($sideSkills as $skill)
         <x-one-skill :skill="$skill" />
