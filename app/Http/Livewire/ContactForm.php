ed656ef76c35880e9cce396ba26012eb3162405a
evgeniyPP <aysanru@gmail.com>
2020-06-21 23:19:27 +0300

    ContactForm validation added

--- app/Http/Livewire/ContactForm.php
+++ app/Http/Livewire/ContactForm.php
@@ -10,24 +10,33 @@ class ContactForm extends Component
 {
     public $name = '';
     public $contact = '';
-    public $message = '';
+    public $text = '';
+
+    private function clearForm()
+    {
+        $this->name = '';
+        $this->contact = '';
+        $this->text = '';
+    }
 
     public function submit()
     {
+        $data = $this->validate([
+            'name' => 'required|string|min:2|max:255',
+            'contact' =>
+            [
+                'required',
+                'regex:/^.+@.+\..+$|\+?\d{11}|\d{10}|\+?\d-\d{3}-\d{3}-\d{2}-\d{2}|\+?\d \d{3} \d{3} \d{2} \d{2}$/'
+            ],
+            'text' => 'required|string|min:10',
+        ]);
         Mail::to('aysanru@gmail.com')
-            ->queue(new ContactLetter($this->name, $this->contact, $this->message));
+            ->queue(new ContactLetter($data));
         $this->clearForm();
     }
 
     public function render()
     {
         return view('components.contact-form');
-    }
-
-    private function clearForm()
-    {
-        $this->name = '';
-        $this->contact = '';
-        $this->message = '';
     }
 }
