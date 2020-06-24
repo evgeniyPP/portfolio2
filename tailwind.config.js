95a49aecb09bac71c9009d21065421712532d95d
evgeniyPP <aysanru@gmail.com>
2020-06-24 12:50:21 +0300

    Public – production version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,17 @@
 module.exports = {
-    purge: ["./resources/views/**/*.blade.php"],
+    purge: {
+        content: ["./resources/views/**/*.blade.php"],
+        options: {
+            whitelist: [
+                "text-yellow-500",
+                "text-blue-400",
+                "text-green-400",
+                "font-normal",
+                "font-semibold",
+                "font-medium"
+            ]
+        }
+    },
     theme: {
         extend: {}
     },
